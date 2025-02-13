df207a120f7ad2d6c628332843524a56c05c5d25
Strangerrrs <166373671+Strangerrrs@users.noreply.github.com>
2025-02-13 21:55:30 +0000

    fixed healthbar for small mobs in MobESP

--- src/main/java/keystrokesmod/module/impl/render/MobESP.java
+++ src/main/java/keystrokesmod/module/impl/render/MobESP.java
@@ -4,17 +4,23 @@ import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.utility.RenderUtils;
 import keystrokesmod.utility.Utils;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.*;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MobESP extends Module {
     private ButtonSetting healthBar;
     private ButtonSetting blaze, creeper, enderman, ghast, silverfish, skeleton, slime, spider, zombie, zombiePigman;
+
+    private final Map<Class<? extends EntityLivingBase>, MobSetting> mobRenders = new HashMap<>();
 
     public MobESP() {
         super("MobESP", category.render);
@@ -29,66 +35,31 @@ public class MobESP extends Module {
         this.registerSetting(spider = new ButtonSetting("Spider §7Black", true));
         this.registerSetting(zombie = new ButtonSetting("Zombie §1Blue", true));
         this.registerSetting(zombiePigman = new ButtonSetting("Zombie Pigman §dPink", true));
+
+        mobRenders.put(EntityBlaze.class, new MobSetting(blaze, Color.orange.getRGB(), 69.0D));
+        mobRenders.put(EntityCreeper.class, new MobSetting(creeper, Color.green.getRGB(), 69.0D));
+        mobRenders.put(EntityEnderman.class, new MobSetting(enderman, Color.black.getRGB(), 106.0D));
+        mobRenders.put(EntityGhast.class, new MobSetting(ghast, Color.white.getRGB(), 143.0D));
+        mobRenders.put(EntitySilverfish.class, new MobSetting(silverfish, Color.gray.getRGB(), 20.0D));
+        mobRenders.put(EntitySkeleton.class, new MobSetting(skeleton, Color.white.getRGB(), 69.0D));
+        mobRenders.put(EntitySlime.class, new MobSetting(slime, Color.green.getRGB()));
+        mobRenders.put(EntitySpider.class, new MobSetting(spider, Color.black.getRGB(), 40.0D));
+        mobRenders.put(EntityCaveSpider.class, new MobSetting(spider, Color.black.getRGB(), 26.0D));
+        mobRenders.put(EntityZombie.class, new MobSetting(zombie, Color.blue.getRGB()));
+        mobRenders.put(EntityPigZombie.class, new MobSetting(zombiePigman, Color.pink.getRGB()));
     }
 
-    private void renderMob(Entity entity, int n) {
-        RenderUtils.renderEntity(entity, 2, 0.0, 0.0, n, false);
+    private void renderMob(EntityLivingBase entity, double height, int rgb, float partialTicks) {
+        RenderUtils.renderEntity(entity, 2, 0.0, 0.0, rgb, false);
         if (healthBar.isToggled()) {
-            RenderUtils.renderEntity(entity, 4, 0.0, 0.0, n, false);
+            drawHealthBar(entity, height, partialTicks);
         }
     }
 
-    private void renderer(final Entity entity) {
-        if (entity instanceof EntityBlaze) {
-            if (!blaze.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.orange.getRGB());
-        } else if (entity instanceof EntityCreeper) {
-            if (!creeper.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.green.getRGB());
-        } else if (entity instanceof EntityEnderman) {
-            if (!enderman.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.black.getRGB());
-        } else if (entity instanceof EntityGhast) {
-            if (!ghast.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, -1);
-        } else if (entity instanceof EntitySilverfish) {
-            if (!silverfish.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.gray.getRGB());
-        } else if (entity instanceof EntitySkeleton) {
-            if (!skeleton.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, -1);
-        } else if (entity instanceof EntitySlime) {
-            if (!slime.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.green.getRGB());
-        } else if (entity instanceof EntitySpider || entity instanceof EntityCaveSpider) {
-            if (!spider.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.black.getRGB());
-        } else if (entity instanceof EntityPigZombie) {
-            if (!zombiePigman.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.pink.getRGB());
-        } else if (entity instanceof EntityZombie) {
-            if (!zombie.isToggled()) {
-                return;
-            }
-            this.renderMob(entity, Color.blue.getRGB());
+    private void renderEntity(EntityLivingBase entity, float partialTicks) {
+        MobSetting mobSetting = mobRenders.get(entity.getClass());
+        if (mobSetting != null && mobSetting.setting.isToggled()) {
+            renderMob(entity, mobSetting.height, mobSetting.color, partialTicks);
         }
     }
 
@@ -102,8 +73,45 @@ public class MobESP extends Module {
                 if (((EntityLivingBase) entity).deathTime != 0) {
                     continue;
                 }
-                this.renderer(entity);
+                renderEntity((EntityLivingBase) entity, e.partialTicks);
             }
+        }
+    }
+
+    private void drawHealthBar(EntityLivingBase en, double mobHeight, float partialTicks) {
+        double x = en.lastTickPosX + (en.posX - en.lastTickPosX) * (double) partialTicks - mc.getRenderManager().viewerPosX;
+        double y = en.lastTickPosY + (en.posY - en.lastTickPosY) * (double) partialTicks - mc.getRenderManager().viewerPosY;
+        double z = en.lastTickPosZ + (en.posZ - en.lastTickPosZ) * (double) partialTicks - mc.getRenderManager().viewerPosZ;
+        GlStateManager.pushMatrix();
+        int xOffset = 21;
+        double health = en.getHealth() / en.getMaxHealth();
+        int height = (int) (mobHeight * health);
+        int healthColor = health < 0.3D ? Color.red.getRGB() : (health < 0.5D ? Color.orange.getRGB() : (health < 0.7D ? Color.yellow.getRGB() : Color.green.getRGB()));
+        GL11.glTranslated(x, y - 0.2D, z);
+        GL11.glRotated(-mc.getRenderManager().playerViewY, 0.0D, 1.0D, 0.0D);
+        GlStateManager.disableDepth();
+        GL11.glScalef(0.03F, 0.03F, 0.03F);
+        net.minecraft.client.gui.Gui.drawRect(xOffset, -1, xOffset + 4, (int) (mobHeight + 1), Color.black.getRGB());
+        net.minecraft.client.gui.Gui.drawRect(xOffset + 1, height, xOffset + 3, (int) mobHeight, Color.darkGray.getRGB());
+        net.minecraft.client.gui.Gui.drawRect(xOffset + 1, 0, xOffset + 3, height, healthColor);
+        GlStateManager.enableDepth();
+        GlStateManager.popMatrix();
+    }
+
+    private static class MobSetting {
+        ButtonSetting setting;
+        int color;
+        double height = 74.0D;
+
+        public MobSetting(ButtonSetting setting, int color) {
+            this.setting = setting;
+            this.color = color;
+        }
+
+        public MobSetting(ButtonSetting setting, int color, double height) {
+            this.setting = setting;
+            this.color = color;
+            this.height = height;
         }
     }
 }
